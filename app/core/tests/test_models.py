8a3d585170505a58cbe00fdf03740d893af21127
Sergey Maslov <sergey.s.maslov@gmail.com>
2021-09-26 16:57:37 +0300

    3

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -28,3 +28,14 @@ class ModelTests(TestCase):
         """ Test create user with bo email raises error """
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test123')
+
+
+    def test_create_new_superuser(self):
+        """ Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@ingos.ru',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
