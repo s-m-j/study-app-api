e216dcf37f178014e5cc9cec0204e4a7dbfc5087
Sergey Maslov <sergey.s.maslov@gmail.com>
2021-10-01 19:53:13 +0300

    Update settings.py
    test

--- app/app/settings.py
+++ app/app/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# Default primary key field type
+# Default primary key field type aaa
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
